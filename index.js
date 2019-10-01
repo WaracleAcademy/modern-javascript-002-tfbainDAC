a4033c4e8631243542103f587e0baa8fe5df9c93
Tracey Bain <t.bain@dundeeandangus.ac.uk>
2019-10-01 18:08:52 +0100

    moving javascript to external file

---
+++ index.js
@@ -0,0 +1,7 @@
+const things=['lemons','oranges','apples','pears'];
+const mainView = document.querySelector('#primaryView');
+mainView.innerHTML = `
+      <h2>Text</h2>
+
+`;
+//console.log("This is Javascript");
